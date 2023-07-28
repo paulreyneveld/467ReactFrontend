8a8cacb01adb74a9d674ebc979e9293f10952a9d
paulreyneveld <63985305+paulreyneveld@users.noreply.github.com>
2023-07-27 20:12:27 -0700

    Create Modular Menu

--- src/components/navigation/desktop/nav-bar-tabs.js
+++ src/components/navigation/desktop/nav-bar-tabs.js
@@ -4,19 +4,39 @@ import { NavBarTab } from "./nav-bar-tab";
 
 export const NavBarTabs = () => {
   const { isAuthenticated } = useAuth0();
+  const { user } = useAuth0();
 
-  return (
-    <div className="nav-bar__tabs">
-      <NavBarTab path="/public" label="Public" />
-      <NavBarTab path="/protected" label="Protected" />
-      {isAuthenticated && (
-        <>
-          <NavBarTab path="/pets" label="Pets" />
-          <NavBarTab path="/addPet" label="Add Pet" />
-          <NavBarTab path="/profile" label="Profile" />
-          <NavBarTab path="/admin" label="Admin" />
-        </>
-      )}
-    </div>
-  );
+  if (user?.role === 'admin') {
+    return (
+      <div className="nav-bar__tabs">
+        <NavBarTab path="/public" label="Public" />
+        <NavBarTab path="/protected" label="Protected" />
+        <NavBarTab path="/pets" label="Pets" />
+        <NavBarTab path="/addPet" label="Add Pet" />
+        <NavBarTab path="/profile" label="Profile" />
+        <NavBarTab path="/admin" label="Admin" />
+      </div>
+    );
+  }
+  else if (user?.role === 'user') {
+    return (
+      <div className="nav-bar__tabs">
+        <NavBarTab path="/public" label="Public" />
+        <NavBarTab path="/protected" label="Protected" />
+        <NavBarTab path="/pets" label="Pets" />
+        <NavBarTab path="/profile" label="Profile" />
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="nav-bar__tabs">
+        <NavBarTab path="/public" label="Public" />
+        <NavBarTab path="/pets" label="Pets" />
+      </div>
+    );
+  }
+
+
+
 };
