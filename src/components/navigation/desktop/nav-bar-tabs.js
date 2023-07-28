4743367721a65c756cde5cfb3811566664a61c64
paulreyneveld <63985305+paulreyneveld@users.noreply.github.com>
2023-07-28 15:57:26 -0700

    Add permission based nav

--- src/components/navigation/desktop/nav-bar-tabs.js
+++ src/components/navigation/desktop/nav-bar-tabs.js
@@ -4,19 +4,37 @@ import { NavBarTab } from "./nav-bar-tab";
 
 export const NavBarTabs = () => {
   const { isAuthenticated } = useAuth0();
+  const { user } = useAuth0();
+  console.log(user);
 
-  return (
-    <div className="nav-bar__tabs">
+  if (user?.role === "user") {
+    return (
+      <div className="nav-bar__tabs">
+      <NavBarTab path="/public" label="Public" />
+      <NavBarTab path="/protected" label="Protected" />  
+      <NavBarTab path="/pets" label="Pets" />
+      <NavBarTab path="/profile" label="Profile" />
+      </div>
+    );
+  }
+  else if (user?.role === "admin") {
+    return (
+      <div className="nav-bar__tabs">
+      <NavBarTab path="/pets" label="Pets" />
+      <NavBarTab path="/addPet" label="Add Pet" />
+      <NavBarTab path="/profile" label="Profile" />
+      <NavBarTab path="/admin" label="Admin" />
       <NavBarTab path="/public" label="Public" />
       <NavBarTab path="/protected" label="Protected" />
-      {isAuthenticated && (
-        <>
-          <NavBarTab path="/pets" label="Pets" />
-          <NavBarTab path="/addPet" label="Add Pet" />
-          <NavBarTab path="/profile" label="Profile" />
-          <NavBarTab path="/admin" label="Admin" />
-        </>
-      )}
-    </div>
-  );
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="nav-bar__tabs">
+      <NavBarTab path="/public" label="Public" />
+      <NavBarTab path="/protected" label="Protected" />
+      </div>
+    );
+  }
 };
