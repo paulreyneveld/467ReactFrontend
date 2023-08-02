371cca6b287bb34d436b988d6c0632a66cae5880
Eric Chin <ericychin94@gmail.com>
2023-08-02 01:25:50 -0700

    pet profile page

--- src/pages/pet-profile-page.js
+++ src/pages/pet-profile-page.js
@@ -1,42 +1,50 @@
-import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { CodeSnippet } from "../components/code-snippet";
+import { useLocation, useNavigate } from "react-router-dom";
 import { PageLayout } from "../components/page-layout";
-import { getProtectedResource } from "../services/message.service";
-import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from "@auth0/auth0-react";
+import { useState, useEffect } from "react";
+import {
+  deletePetResource,
+  updatePetResource,
+} from "../services/message.service";
 
 export const PetProfilePage = () => {
-  const [message, setMessage] = useState("");
-  const { getAccessTokenSilently } = useAuth0();
+  const { user, getAccessTokenSilently } = useAuth0();
   const location = useLocation();
+  const navigate = useNavigate();
+  const [petData, setPetData] = useState(location.state);
 
   useEffect(() => {
-    let isMounted = true;
+    setPetData(location.state);
+  }, [location.state]);
 
-    const getMessage = async () => {
-      const accessToken = await getAccessTokenSilently();
-      const { data, error } = await getProtectedResource(accessToken);
-      console.log(location.state);
+  const handlePetUpdateButton = () => {
+    navigate("/updatePetProfile", { state: { petData } });
+  };
 
-      if (!isMounted) {
-        return;
-      }
+  const handleDeletePetProfile = async (id) => {
+    const accessToken = await getAccessTokenSilently();
+    await deletePetResource(accessToken, id);
+    navigate("/pets");
+  };
 
-      if (data) {
-        setMessage(JSON.stringify(location.state));
-      }
-
-      if (error) {
-        setMessage(JSON.stringify(error, null, 2));
-      }
+  const handleAdoptPet = async (id) => {
+    const updatedPet = {
+      availability: "not available",
     };
-
-    getMessage();
-
-    return () => {
-      isMounted = false;
-    };
-  }, []);
+    console.log(updatedPet);
+    const accessToken = await getAccessTokenSilently();
+    const { data, error } = await updatePetResource(
+      accessToken,
+      id,
+      updatedPet
+    );
+    if (data && !error) {
+      setPetData((prevData) => ({
+        ...prevData,
+        availability: updatedPet.availability,
+      }));
+    }
+  };
 
   return (
     <PageLayout>
@@ -44,17 +52,50 @@ export const PetProfilePage = () => {
         <h1 id="page-title" className="content__title">
           Pet Profile Page
         </h1>
+        <img
+          style={{ height: "auto", width: "50%" }}
+          src={petData.images[0]}
+          alt={petData.breed}
+        />
         <div className="content__body">
-          <p id="page-description">
-            <span>
-              This page retrieves the pet information from an
-              external API.
-            </span>
-            <span>
-              <strong>Only registered visitors can access this page.</strong>
-            </span>
-          </p>
-          <CodeSnippet title="Private Message" code={message} />
+          <p>{petData.description}</p>
+
+          <ul>
+            <li>
+              <strong>Type of animal: </strong> {petData.typeAnimal}
+            </li>
+            <li>
+              <strong>Good with children: </strong>{" "}
+              {petData.goodWithChildren ? "Yes" : "No"}
+            </li>
+            <li>
+              <strong>Good with other animals: </strong>{" "}
+              {petData.goodWithAnimals ? "Yes" : "No"}
+            </li>
+            <li>
+              <strong>Needs to be leashed at all times: </strong>{" "}
+              {petData.leashedAllTimes ? "Yes" : "No"}
+            </li>
+            <li>
+              <strong>Availability: </strong> {petData.availability}
+            </li>
+          </ul>
+          {user?.role === "admin" && (
+            <>
+              <button onClick={handlePetUpdateButton}>Update</button>
+              <button onClick={() => handleDeletePetProfile(petData.id)}>
+                Delete
+              </button>
+            </>
+          )}
+          {user?.role === "user" && (
+            <button
+              onClick={() => handleAdoptPet(petData.id)}
+              disabled={petData.availability.toLowerCase() !== "available"}
+            >
+              Adopt
+            </button>
+          )}
         </div>
       </div>
     </PageLayout>
